1be7a6ed42a06226af823db45e2a7fe41cdcfabd
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-07-12 12:25:56 +0900

    separate componen and inclement props data

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import ListItem from './components/ListItem'
 
 const styles = StyleSheet.create({
   container: {
@@ -9,53 +10,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 22,
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray',
-  },
 })
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 98 }}
-            source={{
-              uri: 'https://picsum.photos/200/200',
-            }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </Text>
-          <Text style={styles.subText}>React News</Text>
-        </View>
-      </View>
+      <ListItem
+        imageUrl={'https://picsum.photos/200/200'}
+        title={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat.'
+        }
+        author={'React News'}
+      />
+      <ListItem
+        imageUrl={'https://picsum.photos/200/200'}
+        title={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat.'
+        }
+        author={'React News'}
+      />
+      <ListItem
+        imageUrl={'https://picsum.photos/200/200'}
+        title={
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat.'
+        }
+        author={'React News'}
+      />
     </View>
   )
 }
