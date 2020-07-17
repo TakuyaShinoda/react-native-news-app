891c74032568570e6bd980dde1f847dfaf1da489
TakuyaShinoda <takuuuya.s6114@gmail.com>
2020-07-17 20:05:37 +0900

    implement webview

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, SafeAreaView, Text } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 const styles = StyleSheet.create({
   container: {
@@ -8,10 +9,12 @@ const styles = StyleSheet.create({
   },
 })
 
-const ArticleScreen = () => {
+const ArticleScreen = ({ route }) => {
+  const { article } = route.params
+  console.log(article)
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is article scrern.</Text>
+      <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   )
 }
